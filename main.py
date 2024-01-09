f7e5cac55718f73f1a4766c3bed3f1186e171389
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-09 17:51:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
-authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('Login', 'main')
 
 if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main', key='unique_key')
