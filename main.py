c33439bc01d952891bd8060fe36820f282a7a866
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-09 17:34:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = Authenticate(
+authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
