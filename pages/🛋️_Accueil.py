2261424e314259f7c022dc9a8957d0a65a45b5a2
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-10 14:36:03 +0100

    Update 🛋️_Accueil.py

--- pages/🛋️_Accueil.py
+++ pages/🛋️_Accueil.py
@@ -27,7 +27,7 @@ def display_rankings():
     rankings = get_rankings()
     
     # Définir un expander dans la barre latérale pour le classement
-    with st.write("Classement"):
+    with st.sidebar.expender("Classement"):
         # Limiter l'affichage à 10 entrées
         top_rankings = rankings[:10]
         
