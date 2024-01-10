1b885930cfead53db95330ae14800281473f97d7
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-10 14:48:28 +0100

    Update 🛋️_Accueil.py

--- pages/🛋️_Accueil.py
+++ pages/🛋️_Accueil.py
@@ -17,6 +17,11 @@ st.set_page_config(
 
 db = init_firestore()
 
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
