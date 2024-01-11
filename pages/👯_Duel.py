831c678f19be12e7e4700c820c87c8467f753f52
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-11 01:27:58 +0100

    Update 👯_Duel.py

--- pages/👯_Duel.py
+++ pages/👯_Duel.py
@@ -31,17 +31,16 @@ def afficher_sessions_duel():
 # Affichage dans la barre latérale pour créer une session
 if 'session_creee' not in st.session_state:
     mode_choisi = st.sidebar.selectbox("Choisir le mode de jeu", ["cyber", "litterature", "geographie", "science", "Extrème"])
-    if mode_choisi == "Duel":
+    nom_session = st.sidebar.text_input("Nom de la session")
+    if st.sidebar.button("Créer une session Duel"):
         nom_session = st.sidebar.text_input("Nom de la session")
-        if st.sidebar.button("Créer une session Duel"):
-            nom_session = st.sidebar.text_input("Nom de la session")
-            nom_utilisateur = auth.name  # Assurez-vous que auth.name est correctement défini
-            mode_de_jeu = st.sidebar.selectbox("Choisir le mode de jeu", ["cyber", "litterature", "science", "geographie", "Extrème"])
+        nom_utilisateur = auth.name  # Assurez-vous que auth.name est correctement défini
+        mode_de_jeu = st.sidebar.selectbox("Choisir le mode de jeu", ["cyber", "litterature", "science", "geographie", "Extrème"])
 
-            if st.sidebar.button("Créer une session Duel"):
-                creer_session_duel(nom_session, nom_utilisateur, mode_de_jeu)
-                st.session_state['session_creee'] = True
-                st.sidebar.write("Session créée avec succès.")
+        if st.sidebar.button("Créer une session Duel"):
+            creer_session_duel(nom_session, nom_utilisateur, mode_de_jeu)
+            st.session_state['session_creee'] = True
+            st.sidebar.write("Session créée avec succès.")
 
 # Affichage des sessions disponibles pour rejoindre
 if 'session_rejointe' not in st.session_state:
