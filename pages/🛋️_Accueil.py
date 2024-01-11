802349d2a13ad36fc586bc55549a082dc02f7a6f
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2024-01-11 02:45:25 +0100

    Update 🛋️_Accueil.py

--- pages/🛋️_Accueil.py
+++ pages/🛋️_Accueil.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from firebase_admin import credentials, firestore, initialize_app, get_app, App
-from auth import init_firestore
 import streamlit_authenticator as stauth
 import auth
 
